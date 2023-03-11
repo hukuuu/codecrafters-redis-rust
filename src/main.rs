2bf8c7495d0e448eb83f339b3b520e30cc6775de
Nikolay Karadzhovf <nikolayk@flutterint.com>
2023-03-11 19:40:10 +0200

    ebasi zmiata

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +12,10 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("PONG".as_bytes());
             }
             Err(e) => {
                 println!("error: {}", e);
